0dc9a1c4d1a79a91ed8c0c7592cbdb9bba264124
huazai-ai <62000693+huazai-ai@users.noreply.github.com>
2025-03-08 23:26:00 +0800

    Update inspect.py
    添加echo话题数据

--- rtui_app/app/inspect.py
+++ rtui_app/app/inspect.py
@@ -10,6 +10,8 @@ from ..ros import RosClient, RosEntity, RosEntityType
 from ..screens import RosEntityInspection
 from ..utility import History
 
+import subprocess
+
 warnings.simplefilter("ignore", ResourceWarning)
 
 
@@ -17,12 +19,13 @@ class InspectApp(App):
     _ros: RosClient
     _init_target: RosEntityType
     _history: History[RosEntity] = History(20)
-
+    
     TITLE = "ROS Inspect"
     BINDINGS = [
         Binding("b", "back", "Prev Page", key_display="b"),
         Binding("f", "forward", "Next Page", key_display="f"),
         Binding("r", "reload", "Reload", key_display="r"),
+        Binding("e", "toggle_echo", "Toggle Echo", key_display="e"),
         Binding("q", "quit", "Quit", key_display="q"),
     ]
 
@@ -61,6 +64,26 @@ class InspectApp(App):
 
     def action_reload(self) -> None:
         self.screen.force_update()
+
+    def action_toggle_echo(self) -> None:
+        if entity := self._history.current():
+            if entity.type == RosEntityType.Topic:
+                """启动一个新的 GNOME 终端，并持续打印 self 的值，防止终端自动关闭。"""
+                command = f'ros2 topic echo {entity.name}'
+                title = f"{command}"
+                
+                # 先杀死可能存在的同名终端进程
+                kill_command = f'pkill -f "{title}"'
+                subprocess.run(kill_command, shell=True)
+
+                # _ros: RosClient
+                # _init_target: RosEntityType
+                # _history: History[RosEntity] = History(20)
+                terminal_cmd = f'gnome-terminal --title "{title}" -- bash -c "{command}; read line"'
+                subprocess.Popen(terminal_cmd, shell=True)
+                self.notify(f"Echo {command}")
+            else:
+                self.notify(f"不是一个话题")
 
     async def action_quit(self) -> None:
         await super().action_quit()
